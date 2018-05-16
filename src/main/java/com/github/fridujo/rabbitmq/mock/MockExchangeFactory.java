505553fd66f117d2ac92a8c21cdc5321e0a707c0
ledoyen <ledoyen.loic@gmail.com>
2018-05-16 22:23:13 +0200

    Add Fanout exchange

--- src/main/java/com/github/fridujo/rabbitmq/mock/MockExchangeFactory.java
+++ src/main/java/com/github/fridujo/rabbitmq/mock/MockExchangeFactory.java
@@ -1,11 +1,13 @@
 package com.github.fridujo.rabbitmq.mock;
 
 public class MockExchangeFactory {
-    public static MockExchange build(String exchangeName, String type) {
+    public static BindableMockExchange build(String exchangeName, String type) {
         if ("topic".equals(type)) {
             return new MockTopicExchange(exchangeName);
         } else if ("direct".equals(type)) {
             return new MockDirectExchange(exchangeName);
+        } else if ("fanout".equals(type)) {
+            return new MockFanoutExchange(exchangeName);
         }
         throw new IllegalArgumentException("No exchange type " + type);
     }
