cb1c9a850d76133febb635647c7c4550f0f3874a
ledoyen <ledoyen.loic@gmail.com>
2018-05-16 22:29:32 +0200

    Assert that channel may be closed

---
+++ src/test/java/com/github/fridujo/rabbitmq/mock/ChannelTest.java
@@ -0,0 +1,23 @@
+package com.github.fridujo.rabbitmq.mock;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ChannelTest {
+
+    @Test
+    void close_closes_channel() throws IOException, TimeoutException {
+        try (Connection conn = new MockConnectionFactory().newConnection()) {
+            try (Channel channel = conn.createChannel()) {
+                channel.close();
+                assertThat(channel.isOpen()).isFalse();
+            }
+        }
+    }
+}
