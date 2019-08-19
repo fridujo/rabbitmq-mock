27734d8acd6f2d73a460bde3944fa128528794b7
Shankar Ramya <ramyas.shankar@gmail.com>
2019-08-19 19:55:20 +0530

    Review comments fixed

--- src/main/java/com/github/fridujo/rabbitmq/mock/MockQueue.java
+++ src/main/java/com/github/fridujo/rabbitmq/mock/MockQueue.java
@@ -10,7 +10,6 @@ import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Queue;
